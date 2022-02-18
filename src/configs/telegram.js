a90d68cdf550dd6d288598d12f6e1c23f26bc942
Alexandr Tkachenko <alexandr.tkachenko@example.com>
2022-02-18 12:48:51 +0200

    change constant

--- src/configs/telegram.js
+++ src/configs/telegram.js
@@ -1,5 +1,5 @@
 const telegram = {
-  chatId: process.env.NODE_CHAT_TEST_ID,
+  chatId: process.env.NODE_CHAT_ACORNS_ID,
   testChatId: process.env.NODE_CHAT_TEST_ID,
   botToken: process.env.NODE_BOT_TOKEN,
 }
