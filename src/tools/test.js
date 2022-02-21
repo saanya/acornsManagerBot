fa46cbcaf49315c7442f7585fbe0ff19de726247
Alexandr Tkachenko <alexandr.tkachenko@example.com>
2022-02-21 16:09:52 +0200

    test merchant data

--- src/tools/test.js
+++ src/tools/test.js
@@ -1,7 +1,9 @@
 require('module-alias/register')
 require('dotenv').config()
 const fetch = require('node-fetch')
-
+console.log(
+  `<?xml version="1.0" encoding="UTF-8"?><request version="1.0"><merchant><id>${process.env.merhcantId}</id><signature>${process.env.merhcantPassword}</signature></merchant><data><oper>cmt</oper><wait>0</wait><test>1</test><payment id=""><prop name="sd" value="01.01.2022" /><prop name="ed" value="01.02.2022" /><prop name="card" value="${process.env.merhcantCard}" /></payment></data></request>`,
+)
 fetch('https://api.privatbank.ua/p24api/rest_fiz', {
   method: 'POST',
   headers: {
