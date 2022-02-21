195d9269c82d435c4f961901bb17ac317dfa527c
Alexandr Tkachenko <alexandr.tkachenko@example.com>
2022-02-21 15:38:01 +0200

    test merchant data

--- src/tools/test.js
+++ src/tools/test.js
@@ -10,6 +10,6 @@ fetch('https://api.privatbank.ua/p24api/rest_fiz', {
   body: `<?xml version="1.0" encoding="UTF-8"?><request version="1.0"><merchant><id>${process.env.merhcantId}</id><signature>${process.env.merhcantPassword}</signature></merchant><data><oper>cmt</oper><wait>0</wait><test>0</test><payment id=""><prop name="sd" value="01.01.2022" /><prop name="ed" value="01.02.2022" /><prop name="card" value="${process.env.merhcantCard}" /></payment></data></request>`,
 }).then((response) => {
   console.log('Response status: ' + response.status)
-  console.log(response.text())
+  console.log(await response.text())
   return response.text()
 })
