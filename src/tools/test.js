4c0aad17a8ba9eca0991369bf734e9f91b19dc9d
Alexandr Tkachenko <alexandr.tkachenko@example.com>
2022-02-21 16:01:45 +0200

    test merchant data

--- src/tools/test.js
+++ src/tools/test.js
@@ -7,7 +7,7 @@ fetch('https://api.privatbank.ua/p24api/rest_fiz', {
   headers: {
     'Content-Type': 'text/xml',
   },
-  body: `<?xml version="1.0" encoding="UTF-8"?><request version="1.0"><merchant><id>${process.env.merhcantId}</id><signature>${process.env.merhcantPassword}</signature></merchant><data><oper>cmt</oper><wait>0</wait><test>0</test><payment id=""><prop name="sd" value="01.01.2022" /><prop name="ed" value="01.02.2022" /><prop name="card" value="${process.env.merhcantCard}" /></payment></data></request>`,
+  body: `<?xml version="1.0" encoding="UTF-8"?><request version="1.0"><merchant><id>${process.env.merhcantId}</id><signature>${process.env.merhcantPassword}</signature></merchant><data><oper>cmt</oper><wait>0</wait><test>1</test><payment id=""><prop name="sd" value="01.01.2022" /><prop name="ed" value="01.02.2022" /><prop name="card" value="${process.env.merhcantCard}" /></payment></data></request>`,
 }).then(async (response) => {
   console.log('Response status: ' + response.status)
   let text = await response.text()
